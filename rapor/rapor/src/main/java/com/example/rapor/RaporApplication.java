c29b1ce9d465d58b0a9d9a4c29e8b7a3965a7a4a
zeydek07 <zeynepbusebasoglu@gmail.com>
2024-08-31 23:05:59 +0300

    Add files via upload

---
+++ rapor/rapor/src/main/java/com/example/rapor/RaporApplication.java
@@ -0,0 +1,13 @@
+package com.example.rapor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaporApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaporApplication.class, args);
+	}
+
+}
---
+++ rapor/rapor/src/test/java/com/example/rapor/RaporApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rapor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaporApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
